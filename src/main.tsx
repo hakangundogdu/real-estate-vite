11160924baa7a8d622ce65c04134f9fb842fb807
Hakan Gundogdu <hakan.gundogdu@gmail.com>
2024-09-10 01:13:08 +0300

    tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Router } from "./router/Router.tsx";
 
@@ -13,7 +12,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<Router />
-			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
 	</React.StrictMode>
 );
